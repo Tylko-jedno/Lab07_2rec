4de3232ab89a7368587e54064429b949c3aa16e0
Tylko-jedno <diogkek@gmail.com>
2024-10-20 20:08:21 +0300

    first master commit

---
+++ Lab07_2rec/Lab07_2rec.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High, int i, int j)
+{
+    a[i][j] = Low + rand() % (High - Low + 1);
+    if (j < colCount - 1)
+        Create(a, rowCount, colCount, Low, High, i, j + 1);
+    else if (i < rowCount - 1)
+        Create(a, rowCount, colCount, Low, High, i + 1, 0);
+}
+
+void Print(int** a, const int rowCount, const int colCount, int i, int j)
+{
+    cout << setw(4) << a[i][j];
+    if (j < colCount - 1)
+        Print(a, rowCount, colCount, i, j + 1);
+    else if (i < rowCount - 1)
+    {
+        cout << endl;
+        Print(a, rowCount, colCount, i + 1, 0);
+    }
+    else
+        cout << endl << endl;
+}
+
+int SearchMaxSum(int** a, const int rowCount, const int colCount, int i, int j, int s, int tmp)
+{
+    if (i >= rowCount)
+        return s;
+
+    if (j < colCount)
+    {
+        if (a[i][j] > tmp)
+            tmp = a[i][j];
+        return SearchMaxSum(a, rowCount, colCount, i, j + 1, s, tmp);
+    }
+    else
+    {
+        s += tmp; 
+        if (i + 1 < rowCount)
+            tmp = a[i + 1][0]; 
+        return SearchMaxSum(a, rowCount, colCount, i + 1, 0, s, tmp);
+    }
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int k;
+    int n;
+
+    cout << "k = "; cin >> k;
+    cout << "n = "; cin >> n;
+    cout << endl;
+
+    int** a = new int* [k];
+
+    for (int i = 0; i < k; i++)
+        a[i] = new int[n];
+
+    int Low = -20, High = 20;
+
+    Create(a, k, n, Low, High, 0, 0);
+    Print(a, k, n, 0, 0);
+
+    int maxSum = SearchMaxSum(a, k, n, 0, 0, 0, a[0][0]);
+
+    cout << "Maximum sum = " << maxSum << endl;
+
+    for (int i = 0; i < k; i++)
+        delete[] a[i];
+
+    delete[] a;
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,58 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <iostream>
+#include "../Lab07_2rec/Lab07_2rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+int** CreateArray(const int rowCount, const int colCount) {
+    int** a = new int* [rowCount];
+    for (int i = 0; i < rowCount; i++)
+        a[i] = new int[colCount];
+    return a;
+}
+
+void DeleteArray(int** a, const int rowCount) {
+    for (int i = 0; i < rowCount; i++)
+        delete[] a[i];
+    delete[] a;
+}
+
+namespace ArrayTest
+{
+    TEST_CLASS(ArrayTest)
+    {
+    public:
+
+        TEST_METHOD(CreateTest)
+        {
+            int rowCount = 3, colCount = 4;
+            int** a = CreateArray(rowCount, colCount);
+            Create(a, rowCount, colCount, 1, 10, 0, 0);
+
+            for (int i = 0; i < rowCount; i++) {
+                for (int j = 0; j < colCount; j++) {
+                    Assert::IsTrue(a[i][j] >= 1 && a[i][j] <= 10);
+                }
+            }
+
+            DeleteArray(a, rowCount);
+        }
+        TEST_METHOD(TestSearchMaxSum)
+        {
+            const int k = 2;
+            const int n = 4;
+            int** a = new int* [k];
+            a[0] = new int[n] { -1, -2, -3, 4 };
+            a[1] = new int[n] { 5, -6, -7, 8 };
+
+            int result = SearchMaxSum(a, k, n, 0, 0, 0, a[0][0]);
+            Assert::AreEqual(12, result);
+
+            for (int i = 0; i < k; i++)
+                delete[] a[i];
+            delete[] a;
+        }
+    };
+}
